24e101ba3af11921efc7181c9006a7220b632d53
Igbineweka Vincent <ifyvinz@gmail.com>
2024-12-07 13:02:54 +0100

    db host name

--- portfolio_backend/portfolio_backend/settings.py
+++ portfolio_backend/portfolio_backend/settings.py
@@ -118,8 +118,8 @@ DATABASES = {
         'NAME': os.getenv('POSTGRES_DB'),
         'USER': os.getenv('POSTGRES_USER'),
         'PASSWORD': os.getenv('POSTGRES_PASSWORD'),
-        'HOST': os.getenv('POSTGRES_HOST', 'db'), # Name of the Docker service for the database
-        'PORT': os.getenv('POSTGRES_PORT', '5432'), # Default PostgreSQL port
+        'HOST': 'db', # Name of the Docker service for the database
+        'PORT': '5432', # Default PostgreSQL port
     }
 }
 
